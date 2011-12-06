1640fe75ae36556f4c0f9e03a1942eab9dca494b
Bryan Moyles <bmoy117@gmail.com>
2011-12-06 09:58:36 -0500

    Completely revamped the TQL parser for increased recursion capabilities, and also added LIKE and NOT LIKE support!

--- tql/__init__.py
+++ tql/__init__.py
@@ -14,39 +14,6 @@ def parse(raw_sql, allowed_models):
         "!="    : "exact"
     }
 
-    raw_sql = re.sub(r'\s\'([^\s])\'\s?', r' "\1"', raw_sql)
-
-    print "RAW SQL: %s" % raw_sql
-
-    token_list = sqlparse.parse(raw_sql)[0]
-    
-    def gather_search_part(comparison_token_list):
-        print "Comparison List: %s" % comparison_token_list.tokens
-
-        search_token = comparison_token_list.token_next_by_instance(0, sqlparse.sql.Identifier)
-
-        print "Search Token: %s" % search_token
-
-        equality_token = comparison_token_list.token_next_by_type(comparison_token_list.token_index(search_token), sqlparse.tokens.Comparison)
-        
-        print "Equality Token: %s" % equality_token
-
-        value_token = comparison_token_list.token_next_by_instance(comparison_token_list.token_index(equality_token), (sqlparse.sql.Identifier, sqlparse.sql.Comment, ))
-        
-        print "Value Token: %s" % value_token
-
-        equality_token_value = equality_token.to_unicode()
-
-        operator = operator_map.get(equality_token_value, '')
-
-        q_search = ("%s__%s" % (search_token.to_unicode(), operator)).encode('utf-8')
-
-        # Needed to replace quotations, as the strings unicoded rendition is encased by them
-        if equality_token_value == "!=":
-            return ~Q(**{q_search:value_token.to_unicode().replace('\"','')})
-        else:
-            return Q(**{q_search:value_token.to_unicode().replace('\"','')})
-
     def get_joining_operator(token_list, index):
         conjunction_token = token_list.token_next_by_type(index, sqlparse.tokens.Keyword)
 
@@ -74,58 +41,114 @@ def parse(raw_sql, allowed_models):
                 return allowed_model
         
         return None
+    
+    def gaaather_search_part(key_token, equality_token, value_token, negate = False):
+        print "NEGATE? : %s" % negate
+
+        search_value = value_token.to_unicode().replace('\"','')
+
+        if equality_token.match(sqlparse.tokens.Keyword, "LIKE"):
+            start_wildcard = search_value.startswith('%')
+            end_wildcard = search_value.endswith('%')
+
+            search_value = search_value.replace('%', '')
+
+            if start_wildcard and end_wildcard:
+                operator = 'contains'
+            elif start_wildcard:
+                operator = 'endswith'
+            elif end_wildcard:
+                operator = 'startswith'
+            else:
+                operator = 'exact'
+
+            q_search = ("%s__%s" % (key_token.to_unicode(), operator)).encode('utf-8')
+
+        elif equality_token.match(sqlparse.tokens.Comparison, operator_map.keys()):
+            operator = operator_map.get(equality_token.value, '')
+
+            q_search = ("%s__%s" % (key_token.to_unicode(), operator)).encode('utf-8')
+
+        # Needed to replace quotations, as the strings unicoded rendition is encased by them
+        if equality_token.value == "!=" or negate:
+            return ~Q(**{q_search:search_value})
+        else:
+            return Q(**{q_search:search_value})
 
     # WHERE email_address = "123@test.com" OR (email_address = "456@test.com" AND first_name = "Bryan")
-    def iterate_comparisons(token_list):
-        print "Where List!"
+    def iterate_token_list(token_list, q = None):
         print token_list.tokens
 
-        child_list = token_list.token_next_by_instance(0, (sqlparse.sql.Comparison, sqlparse.sql.Parenthesis, ))
+        index = 0
 
-        print "CHILD LIST: %s" % child_list
-        print "CHILD INSTANCE: %s" % child_list.__class__
-
-        q = Q()
-
+        if not q:
+            q = Q()
+        
         joining_operator = None
 
-        while child_list:
-            print "START LOOP" 
+        while True:
+            tokens = [
+                token_list.token_next_match(index, sqlparse.tokens.Keyword, ["LIKE", "NOT"]),
+                token_list.token_next_match(index, sqlparse.tokens.Comparison , operator_map.keys()),
+                token_list.token_next_by_instance(index, sqlparse.sql.Comparison),
+                token_list.token_next_by_instance(index, sqlparse.sql.Parenthesis)
+            ]
 
-            if isinstance(child_list, sqlparse.sql.Comparison):
-                search_part = gather_search_part(child_list)
+            min_index = 9999999999999
+            token = None
 
-                print "SEARCH PART: %s" % search_part
+            for index in range(len(tokens)):
+                if tokens[index]:
+                    current_index = token_list.token_index(tokens[index])
 
-                if joining_operator:
-                    q.add(search_part, joining_operator)
+                    min_index = min(min_index, current_index)
+
+                    if min_index == current_index:
+                        token = tokens[index]
+
+            # if isinstance(token, sqlparse.tokens.Keyword):
+            if token:
+                if token.ttype in [sqlparse.tokens.Keyword, sqlparse.tokens.Comparison]:
+                    index = token_list.token_index(token)
+
+                    if token.value.upper() == 'NOT':
+                        (key_token, token, value_token) = (token_list.token_prev(index), token_list.token_next(index + 1), token_list.token_next(index + 2))
+                    else:
+                        (key_token, value_token) = (token_list.token_prev(index), token_list.token_next(index))
+
+                    q_object = gaaather_search_part(key_token, token, value_token, token.value.upper() == 'NOT')
+
+                    index = token_list.token_index(value_token)
+                elif isinstance(token, sqlparse.sql.Comparison) or isinstance(token, sqlparse.sql.Parenthesis):
+                    q_object = iterate_token_list(token, q)
+
+                    index = token_list.token_index(token)
                 else:
-                    q = search_part
+                    break
+            else:
+                break
+            
+            if joining_operator:
+                q.add(q_object, joining_operator)
+            else:
+                q = q_object
+            
+            conjunction_token, joining_operator = get_joining_operator(token_list, index)
 
-            elif isinstance(child_list, sqlparse.sql.Parenthesis):
-                print "PARENTHESIS"
-
-                if joining_operator:
-                    q.add(iterate_comparisons(child_list), joining_operator)
-                else:
-                    q = iterate_comparisons(child_list)
-
-            print "Defined q: %s " % q
-
-            conjunction_token, joining_operator = get_joining_operator(token_list, token_list.token_index(child_list))
-
-            print "Defined q: %s " % q
-            print "CONJUNCTION_TOKEN %s" % conjunction_token
-
-            # No additional conjunctions discovered, break out of loop
-            if not conjunction_token:
+            if not joining_operator:
                 break
 
-            token_index = token_list.token_index(conjunction_token)
-
-            child_list = token_list.token_next_by_instance(token_index, (sqlparse.sql.Comparison, sqlparse.sql.Parenthesis, ))
+            # Move onto the next element
+            index += 1
         
         return q
+    
+    # Replace single quotations with double quotations
+    raw_sql = re.sub(r'\s\'([^\s])\'\s?', r' "\1"', raw_sql)
+
+    print "RAW SQL: %s" % raw_sql
+
+    token_list = sqlparse.parse(raw_sql)[0]
     
     print token_list.tokens
 
@@ -152,7 +175,7 @@ def parse(raw_sql, allowed_models):
     where_list = token_list.token_next_by_instance(token_list.token_index(next_token), sqlparse.sql.Where)
 
     if where_list:
-        q = iterate_comparisons(where_list)
+        q = iterate_token_list(where_list)
 
         print "FINAL Q: %s" % q
 
